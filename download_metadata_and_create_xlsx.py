e9f45f13d546949414c4979aab00de5544c3f027
snow-mn <mn.daigaku@gmail.com>
2021-05-25 02:53:15 +0900

    test

---
+++ download_metadata_and_create_xlsx.py
@@ -0,0 +1,212 @@
+# 『なろう小説API』を用いて、なろうの『全作品情報データを一括取得する』Pythonスクリプト
+# 取得したメタデータを総合評価順にソート、またキーワードの出現回数もカウントする
+
+import requests
+import pandas as pd
+import numpy as np
+import json
+import time as tm
+import datetime
+import gzip
+from tqdm import tqdm
+import csv
+import openpyxl
+
+tqdm.pandas()
+import xlsxwriter
+import sqlite3
+
+# ディレクトリ
+data_dir = "../novel_Data/"
+text_dir = "../novel_Text/"
+
+# リクエストの秒数間隔(1以上を推奨)
+interval = 2
+
+### なろう小説API・なろう１８禁小説API を設定 ####
+is_narou = True
+
+now_day = datetime.datetime.now()
+now_day = now_day.strftime("%Y_%m_%d")
+
+if is_narou:
+    filename = 'Narou_All_OUTPUT_%s.xlsx' % now_day
+    sql_filename = 'Narou_All_OUTPUT_%s.sqlite3' % now_day
+    api_url = "https://api.syosetu.com/novelapi/api/"
+else:
+    filename = 'Narou_18_ALL_OUTPUT_%s.xlsx' % now_day
+    sql_filename = 'Narou_18_ALL_OUTPUT_%s.sqlite3' % now_day
+    api_url = "https://api.syosetu.com/novel18api/api/"
+
+# データをSqlite3形式でも保存する
+is_save_sqlite = False
+
+
+#####　以上設定、以下関数　##############
+
+# 全作品情報の取得
+def get_all_novel_info():
+    df = pd.DataFrame()
+
+    # 出力パラメータの設定
+    # 総合ポイントの下限と文字数の上限を設定
+    payload = {'out': 'json', 'gzip': 5, 'of': 'n', 'lim': 1, "general_all_no": 2-10000 ,"min_globalpoint": 1000, "length": -500000}
+    # レスポンスをcontent形式で取得
+    res = requests.get(api_url, params=payload).content
+    # 解凍して展開、デコード
+    r = gzip.decompress(res).decode("utf-8")
+    # json文字列を辞書に変換して、対象作品数の値を取得して代入
+    allcount = json.loads(r)[0]["allcount"]
+
+    print('対象作品数  ', allcount);
+
+    # キュー数、500で切り捨て除算
+    all_queue_cnt = (allcount // 500) + 10
+
+    # 現在時刻を取得
+    nowtime = datetime.datetime.now().timestamp()
+    lastup = int(nowtime)
+
+    # 進捗表示しながら繰り返し処理
+    for i in tqdm(range(all_queue_cnt)):
+        payload = {'out': 'json', 'gzip': 5, 'opt': 'weekly', 'lim': 500, 'lastup': "1073779200-" + str(lastup), "general_all_no": 2-10000, "min_globalpoint": 1000, "length": -500000}
+
+        # なろうAPIにリクエスト
+        cnt = 0
+        while cnt < 5:
+            try:
+                res = requests.get(api_url, params=payload, timeout=30).content
+                break
+            except:
+                print("Connection Error")
+                cnt = cnt + 1
+                tm.sleep(120)  # 接続エラーの場合、120秒後に再リクエストする
+
+        r = gzip.decompress(res).decode("utf-8")
+
+        # pandasのデータフレームに追加する処理
+        df_temp = pd.read_json(r)
+        df_temp = df_temp.drop(0)
+
+        df = pd.concat([df, df_temp])
+
+        last_general_lastup = df.iloc[-1]["general_lastup"]
+
+        lastup = datetime.datetime.strptime(last_general_lastup, "%Y-%m-%d %H:%M:%S").timestamp()
+        lastup = int(lastup)
+
+        # 取得間隔を空ける
+        tm.sleep(interval)
+
+    dump_to_excel(df)
+
+
+# エクセルファイルに書き込む処理
+def dump_to_excel(df):
+    # allcount列を削除
+    df = df.drop("allcount", axis=1)
+
+    # 重複行を削除する
+    df.drop_duplicates(subset='ncode', inplace=True)
+    df = df.reset_index(drop=True)
+
+    print("export_start", datetime.datetime.now())
+
+    try:
+        # .xlsx ファイル出力
+        writer = pd.ExcelWriter(data_dir + filename, options={'strings_to_urls': False}, engine='xlsxwriter')
+        df.to_excel(writer, sheet_name="Sheet1")  # Writerを通して書き込み
+        writer.close()
+
+        print('取得成功数  ', len(df));
+
+    except:
+        pass
+
+    ### SQLite3に書き込む処理 (将来エクセルの上限行数を超えたときのため) ###
+    if is_save_sqlite == True or len(df) >= 1048576:
+        # 接続DBファイルの指定
+        conn = sqlite3.connect(sql_filename)
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
+
+        df.to_sql('novel_data', conn, if_exists='replace')
+
+        c.close()
+        conn.close()
+
+        print("Sqlite3形式でデータを保存しました")
+
+    sort_excel(data_dir + filename)
+
+
+# メタデータファイルを総合評価ポイントの降順にソート
+def sort_excel(file_path):
+    # DataFrameにExcelの読み込み
+    df = pd.read_excel(file_path, sheet_name='Sheet1', engine='openpyxl')
+
+    # Z列のglobal_pointを基準にして降順(False)・昇順（True）ができる
+    df_a = df.sort_values('global_point', ascending=False)
+
+    # ExcelWriterを追加することで、SaveとCloseの必要がなくなる。またengineをopenpyxlに指定ができる
+    with pd.ExcelWriter(file_path, engine='openpyxl', mode='a') as writer:
+        # A列の国民の祝日・休日月日を基準にして降順(False)にしたデータをresultシートに追加
+        df_a.to_excel(writer, sheet_name='sorted', index=False)
+
+    count_word_ranking(file_path)
+
+
+# キーワードの出現回数のランキングを調べる
+def count_word_ranking(file_path):
+    # エクセルデータの取得
+    wb = openpyxl.load_workbook(file_path)
+    ws = wb["Sheet1"]
+
+    # キーワードと出現回数を入れる辞書の作成
+    keywords_dict = {}
+
+    # 2行目から1行ごとに読み込む
+    for row in ws.iter_rows(min_row=2):
+        # キーワードの取得
+        keywords = row[9].value
+        # キーワードが存在しないならパス
+        if keywords is None:
+            # print("キーワードがありませんでした")
+            pass
+        # キーワドが存在するなら
+        else:
+            # " "を区切り文字として分割してリスト化
+            keywords_list = keywords.split(" ")
+            # print(keywords_list)
+            # 辞書内の各キーワードについて
+            for key in keywords_list:
+                # print(key)
+                # 辞書内にキーワードが含まれていなければ追加、含まれていれば回数+1
+                if key in keywords_dict:
+                    keywords_dict[key] = keywords_dict[key] + 1
+                else:
+                    keywords_dict[key] = 1
+
+    # print(keywords_dict)
+    # 出現回数降順にソート
+    # keywords_dict = {key, count for key, count in keywords_dict.items() if count >= 10000}
+    sorted_dict = sorted(keywords_dict.items(), key=lambda x: x[1], reverse=True)
+    # print(sorted_dict)
+
+    # ファイルパス
+    keywordcount_xlsx_path = data_dir + "keyword_count_%s.xlsx" % now_day
+    # データフレーム化
+    df = pd.DataFrame(sorted_dict[0:200], columns=["keyword", "count"])
+    print(df)
+    # エクセルファイルを作成・保存
+    df.to_excel(keywordcount_xlsx_path, sheet_name="keyword_count")
+    print(keywordcount_xlsx_path + "の保存が完了しました")
+
+
+
+#######　関数の実行を指定　##########
+print("start", datetime.datetime.now())
+
+get_all_novel_info()
+
+print("end", datetime.datetime.now())
---
+++ narou_text_download.py
@@ -0,0 +1,121 @@
+import os
+import csv
+import time
+import re
+from urllib import request
+from urllib import error
+from bs4 import BeautifulSoup
+import openpyxl
+from tqdm import tqdm
+
+data_dir = "../novel_Data/"
+text_dir = "../novel_Text/"
+narou_data_path = data_dir + "Narou_All_OUTPUT_2021_05_04.xlsx"
+csv_path = data_dir + "keyword_count_2021_05_04.csv"
+
+
+# これを最初に呼び出す
+def first_function(csv_path):
+    csv_file = open(csv_path, "r", errors="", newline="")
+    f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"',
+                   skipinitialspace=True)
+    header = next(f)
+    # print(header)
+    count = 0
+    for row in f:
+        count += 1
+        if count > 100:
+            exit()
+        else:
+            # キーワードのディレクトリが存在していないなら作成
+            if os.path.exists(text_dir + row[0]):
+                pass
+            else:
+                make_dir(row[0])
+            # キーワードディレクトリ内に既に100個のファイルがあるならパス
+            if sum(os.path.isfile(os.path.join(text_dir + row[0], name)) for name in os.listdir(text_dir + row[0])) < 100:
+                # rowはList
+                # row[0]で必要な項目を取得することができる
+                second_function(row[0])
+            else:
+                pass
+
+
+# ディレクトリ生成
+def make_dir(keyword):
+    path = text_dir + keyword
+    os.mkdir(path)
+
+
+# 2段階目の関数
+# なろうメタデータファイルからメタデータを取得して小説本文のダウンロード
+def second_function(keyword):
+    # エクセルデータファイルの取得
+    wb = openpyxl.load_workbook(narou_data_path)
+    ws = wb["sorted"]
+    # 行ごとに取得
+    for row in tqdm.tqdm(ws.iter_rows(min_row=2)):
+        # 各種メタデータの値の取得
+        title = row[1].value
+        ncode = row[2].value
+        keyword_text = row[9].value
+        general_all_no = row[14].value
+        length = row[15].value
+
+        # キーワードが存在するなら分割
+        if keyword_text is not None:
+            keywords = keyword_text.split(" ")
+
+            # 該当キーワードが付与されている小説かつ掲載部分数が1より大きい（短編ではない）
+            # 文字数に条件を設けるか
+            if keyword in keywords and general_all_no > 1 and length < 100000:
+                # ディレクトリ内のファイル数が100を超えないように
+                if sum(os.path.isfile(os.path.join(text_dir + keyword, name)) for name in os.listdir(text_dir + keyword)) < 100:
+                    if os.path.exists(text_dir + keyword + "/" + ncode + ".txt"):
+                        pass
+                    else:
+                        print(ncode + ":" + title + "のダウンロードを開始します")
+                        fetch_novel(keyword, ncode)
+                        print(ncode + ":" + title + "のダウンロードが終了しました")
+                else:
+                    pass
+            else:
+                pass
+        else:
+            pass
+
+
+
+# 小説本文の取得
+def fetch_novel(keyword, ncode):
+    # 例外処理
+    try:
+        # 全部分数を取得
+        info_url = "https://ncode.syosetu.com/novelview/infotop/ncode/{}/".format(ncode)
+        info_res = request.urlopen(info_url)
+        soup = BeautifulSoup(info_res, "html.parser")
+        pre_info = soup.select_one("#pre_info").text
+        num_parts = int(re.search(r"全([0-9]+)部分", pre_info).group(1))
+        # 本文取得
+        with open(text_dir + keyword + "/" + ncode + ".txt", "w", encoding="utf-8") as f:
+            for part in range(1, num_parts + 1):
+                # 作品本文ページのURL
+                url = "https://ncode.syosetu.com/{}/{:d}/".format(ncode, part)
+
+                res = request.urlopen(url)
+                soup = BeautifulSoup(res, "html.parser")
+
+                # CSSセレクタで本文を指定
+                honbun = soup.select_one("#novel_honbun").text
+                honbun += "\n"  # 次の部分との間は念のため改行しておく
+
+                # 保存
+                f.write(honbun)
+
+                print("part {:d} downloaded (total: {:d} parts)".format(part, num_parts))  # 進捗を表示
+
+                time.sleep(0.1)  # 次の部分取得までは1秒間の時間を空ける
+    except error.HTTPError as e:
+        print("エラー : ", e)
+
+first_function(csv_path)
